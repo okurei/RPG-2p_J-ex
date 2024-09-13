a32a4d6e7d294a103b8019d9e74e7db458f5d14f
okurei <167979336+okurei@users.noreply.github.com>
2024-09-13 11:08:52 +0200

    modifiche alle logiche

--- src/PrintClass.java
+++ src/PrintClass.java
@@ -7,6 +7,7 @@ public class PrintClass {
     public void printFight(int hit, int cHit, int dmg){
         System.out.printf(" attack (hit %d crit %d)\nDeal %d damage", hit, cHit, dmg);
     }
+
     public void win (String name){
         System.out.printf("%s", name);
     }
