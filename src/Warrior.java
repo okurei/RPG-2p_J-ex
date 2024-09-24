a5318d14ab21d486207bac11e542a5be766f9433
okurei <167979336+okurei@users.noreply.github.com>
2024-09-24 13:25:41 +0200

    Different job version

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,5 +1,6 @@
 public class Warrior extends Character {
     int hp = 120;
+    int baseAtk = 9;
     public Warrior(String name) {
         super(name);
     }
@@ -13,5 +14,10 @@ public class Warrior extends Character {
     public void setHp(int dmg) {
         this.hp = hp - dmg;
         if(hp < 0){this.hp = 0;}
+    }
+
+    @Override
+    public int getBaseAtk(){
+        return baseAtk;
     }
 }
