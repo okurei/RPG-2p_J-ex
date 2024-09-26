2493859ac89d08df2308c5aa5491d4ecfedd4471
okurei <167979336+okurei@users.noreply.github.com>
2024-09-26 11:03:39 +0200

    optimization

--- src/Combat.java
+++ src/Combat.java
@@ -8,14 +8,14 @@ public class Combat {
     Random rdm = new Random();
 
     /**
-     * the main function that handle the fight*/
+     * handle the initiative for each turn*/
     public void fight(){
         int turn = 1;
         do{
             int initiative = rdm.nextInt(2)+1; /* set first to attack each turn*/
             printC.turnOrder(turn);
             turn++;
-            /*if statement for the initiative*/
+            /*ternary statement for the initiative*/
             handleFight(initiative == 1 ? char1 : char2, initiative == 1 ? char2 : char1);
 
         }while(char1.getHp() > 0 && char2.getHp() > 0);/*do while both the player have hp above 0 */
@@ -23,7 +23,8 @@ public class Combat {
         printC.win(char2.getHp() == 0 ? char1.getName(): char2.getName());
     }
 
-
+/**
+ * call the damage and the print for the fight*/
     private void handleFight(Character character1, Character character2){
         int dmg = printC.printFight(character1);
         character2.setHp(dmg);
